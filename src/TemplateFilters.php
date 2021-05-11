b1544cdd4ef0297999072326be68761ded2da18c
Petr Parolek <petr.parolek@gmail.com>
2021-05-11 17:53:03 +0200

    bugfixes - Nette 3.1 is not compatible - code improvements reported by PHPStan

--- src/TemplateFilters.php
+++ src/TemplateFilters.php
@@ -20,6 +20,8 @@ class TemplateFilters
 
 			return ($callback)($args);
 		}
+
+		return null;
 	}
 
 
@@ -29,7 +31,7 @@ class TemplateFilters
 		$text = nl2br($text);
 		$text = str_replace(["\n", "\n\r", "\r\n", "\r"], '', $text);
 
-		$text = preg_replace_callback('/<json><br \/>(.*?)<\/json>/s', function ($item) {
+		$text = preg_replace_callback('/<json><br \/>(.*?)<\/json>/s', function ($item): string {
 			$s = '<br><pre class="apiDocu-json">' . str_replace('<br>', '', end($item)) . '</pre>';
 			$s = (string) preg_replace('/(\s)"([^"]+)"/', '$1<span class="apiDocu-string">"$2"</span>', $s);
 			$s = (string) preg_replace('/\/\/(.*?)<br \/>/', '<span class="apiDocu-comment">//$1</span><br>', $s);
